8f831e4efe389ca120b23b3d3f92b544874ae49b
luisr97 <lraygada@pnw.edu>
2020-01-29 01:00:22 -0600

    * App1.Android.csproj: Using this commit to test Version control
    * Styles.css:
    * MainPage.xaml.cs:
    * AndroidManifest.xml:

--- SchedulingApp/App1/App1/App1/MainPage.xaml.cs
+++ SchedulingApp/App1/App1/App1/MainPage.xaml.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace App1
+namespace App1 
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
